b216fae2edb1985ef037e0894d391bd045ec1173
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-02-15 17:31:44 +0000

    Basic UI Structure added
    Added the main screen of the app. Need to add more buttons and functionality.

--- src/view/CalculatorApp.java
+++ src/view/CalculatorApp.java
@@ -1,9 +1,33 @@
 package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * This will be there main class where the Business Logic will be.
  * @author zacharyishmael
  *
  */
-public class CalculatorApp {
+public class CalculatorApp extends Application {
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Calculator App");
+		Parent root = FXMLLoader.load(getClass().getResource("/view/CalculatorMain.fxml"));
+		Scene scene = new Scene(root, 600, 400);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main (String [] args) {
+		launch(args);
+	}
+
+	
+	
 }
---
+++ src/view/FxController.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class FxController {
+
+    @FXML
+    private HBox hboxCentre;
+
+    @FXML
+    private HBox hboxTop;
+
+    @FXML
+    private VBox hboxLeft;
+
+}
