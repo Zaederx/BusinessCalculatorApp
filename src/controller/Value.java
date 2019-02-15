81d9db9472139f86999c39cd76eb90e435029e51
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-02-15 15:25:07 +0000

    Added the Main ratios
    Added the main ratios need to run the calculator

---
+++ src/controller/Value.java
@@ -0,0 +1,11 @@
+package controller;
+/**
+ * A class used to work out the present and future value
+ * of a given amount over time.
+ * @author zacharyishmael
+ *
+ */
+
+public class Value {
+
+}
--- src/view/CalculatorApp.java
+++ src/view/CalculatorApp.java
@@ -1,5 +1,9 @@
 package view;
-
+/**
+ * This will be there main class where the Business Logic will be.
+ * @author zacharyishmael
+ *
+ */
 public class CalculatorApp {
 
 }
