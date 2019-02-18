551a93a15190edd34b4d6ce11864ca22d915260e
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-02-18 19:19:57 +0000

    Added the main view and images
    Added the main view of the app with some images. Also added value calls variables with getters and setters.

--- src/controller/Value.java
+++ src/controller/Value.java
@@ -8,4 +8,47 @@ package controller;
 
 public class Value {
 
+/**
+ * pv = present value
+ * 
+ */
+ double pv = 0.0;
+ 
+ /**
+  * fv = future value
+  */
+ double fv = 0.0;
+
+/* returns pv
+ * @return double pv = present value
+ */
+public double getPv() {
+	return pv;
+}
+
+/**
+ * sets the value of pv.
+ * @param pv
+ */
+public void setPv(double pv) {
+	this.pv = pv;
+}
+
+/**
+ * returns the value of pv.
+ * @return int fv - future value
+ */
+public double getFv() {
+	return fv;
+}
+
+/**
+ * sets the value of fv.
+ * @param fv - future value
+ */
+public void setFv(double fv) {
+	this.fv = fv;
+}
+ 
+ 
 }
